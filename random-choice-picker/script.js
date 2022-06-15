e02fb9169884a89705141a456a05ba6b1ed04589
luojiyin <luojiyin@hotmail.com>
2022-06-15 10:52:11 +0800

    feat: random choice

---
+++ random-choice-picker/script.js
@@ -0,0 +1,74 @@
+
+
+const tagsEl = document.getElementById('tags');
+const textArea = document.getElementById('textarea');
+console.log('tagsEl', tagsEl);
+console.log('textArea', textArea);
+
+textArea.focus()
+textArea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+    console.log("keyup", e.value);
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10)
+        randomSelect()
+    }
+})
+
+
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.innerHTML = tag;
+        tagEl.classList.add('tag');
+        tagsEl.appendChild(tagEl);
+    })
+}
+
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const radomTag = pickRandomTag();
+        if (radomTag !== undefined) {
+            highlightTag(radomTag);
+
+            setTimeout(() => {
+                unHighlightTag(radomTag);
+            }, 100)
+        }
+
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100)
+    }, times * 100);
+}
+
+
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
+}
+
