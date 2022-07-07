863cf0f3324327b4a8da0bb055bdddcad8ef9f7d
luojiyin <luojiyin@hotmail.com>
2022-07-07 10:58:54 +0800

    del log

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -2,32 +2,32 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 
 const text = 'We Love Programming'
-let idx =1 
-let speed = 300 /speedEl.value 
+let idx = 1
+let speed = 300 / speedEl.value
 
 displayText()
 
-speedEl.addEventListener('input', (e)=> {
-    console.log('e.target.value', e.target.value)
-    if(e.target.value > 10) {
+speedEl.addEventListener('input', (e) => {
+
+    if (e.target.value > 10) {
         e.target.value = 10
     }
 
-    if(!e.target.value) {
-        e.target.value =1
+    if (!e.target.value) {
+        e.target.value = 1
     }
-    console.log('end e.target.value', e.target.value)
-    speed =300 / e.target.value 
+
+    speed = 300 / e.target.value
 })
 
 
 
 function displayText() {
     textEl.innerText = text.slice(0, idx)
-    idx ++
+    idx++
 
-    if(idx >text.length) {
-        idx =0
+    if (idx > text.length) {
+        idx = 0
     }
 
     setTimeout(displayText, speed)
