d491fa366007384a1400d433135923fb90bf4863
luojiyin <luojiyin@hotmail.com>
2022-07-21 11:32:21 +0800

    code clear

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -8,11 +8,11 @@ codes.forEach((code, idx) => {
         if (e.key >= 0 && e.key <= 9 ) {
             codes[idx].value = ''
            
-            setTimeout(() => { codes[idx + 1===length?idx:idx+1].focus() }, 10)
+            setTimeout(() => { codes[idx===length-1?idx:idx+1].focus() }, 10)
         } else if (e.key === 'Backspace' ) {
             codes[idx].value = ''
             
-            setTimeout(() => { codes[ idx - 1===-1?idx: idx-1].focus() }, 10)
+            setTimeout(() => { codes[ idx === 0?idx: idx-1].focus() }, 10)
         }
     })
 })
