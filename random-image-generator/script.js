41a467bfd48b81a8689c19b8951f5612d35e75ea
luojiyin <luojiyin@hotmail.com>
2022-07-29 10:54:18 +0800

    feat: scroll load image

--- random-image-generator/script.js
+++ random-image-generator/script.js
@@ -2,16 +2,35 @@ const container = document.querySelector('.container')
 const unsplashURL = 'https://source.unsplash.com/random/'
 const rows = 5
 
-for(let i=0; i < rows *3; i++) {
-    const img = document.createElement('img')
-    img.src= `${unsplashURL}${getRandomSize()}`
-    container.appendChild(img)
+function randomImage() {
+
+    for (let i = 0; i < rows * 3; i++) {
+        const img = document.createElement('img')
+        img.src = `${unsplashURL}${getRandomSize()}`
+        container.appendChild(img)
+    }
 }
 
 function getRandomSize() {
-    return getRandomNr() + 'x'+ getRandomNr()
+    return getRandomNr() + 'x' + getRandomNr()
 }
 
 function getRandomNr() {
     return Math.floor(Math.random() * 100) + 300
 }
+
+
+
+
+
+window.addEventListener('scroll', () => {
+    let scrollHeight = document.documentElement.scrollHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    let clientHeight = document.documentElement.clientHeight;
+
+    if ((scrollTop + clientHeight) > (scrollHeight - 5)) {
+        setTimeout(randomImage, 1000);
+    }
+})
+
+randomImage()
